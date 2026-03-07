c3cedebf39c722a563f393be6ae07129a35dfd7b
何思瑶 <hesiyao@hesiyaodeMacBook-Air.local>
2026-03-07 23:39:20 +0000

    deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  base: "/mcti-storm-dashboard/",
   plugins: [react()],
   server: {
     port: 5173,
   },
 });
-
